bd7a8982665747bbbe73875e5395edd9aa4a5109
Chris7Ford <cford2132@gmail.com>
2018-02-04 21:31:18 -0600

    Change size of board + begin initialize screen
    Changed the size of the board so the images fit better Put divs inside
    of each table cell, so border is not affected by opacity Began an
    initialize screen, but it does not work yet.  Will revisit tomorrow

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-var w1 = document.getElementsByClassName("w1");
-var w2 = document.getElementsByClassName("w2");
-var w3 = document.getElementsByClassName("w3");
-var y1 = document.getElementsByClassName("y1");
-var y2 = document.getElementsByClassName("y2");
-var y3 = document.getElementsByClassName("y3");
-var z1 = document.getElementsByClassName("z1");
-var z2 = document.getElementsByClassName("z2");
-var z3 = document.getElementsByClassName("z3");
+//beginning script for the 'initializing screen'
+var initialize = document.querySelector('.initialize');
+var button = document.querySelector('.button');
+
+
+button.addEventListener('click', function(){
+initialize.classList.toggle('hidden');
+});
