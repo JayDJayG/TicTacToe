952e4eb82a1cf77b0f706e8ccbf06fa7be1f32e5
Jay Guzman <j.d.guzman.v@gmail.com>
2018-02-06 10:05:10 -0700

    Implemented a flexbox solution in order to be able to center the board, while adding an square outside the board that will help us as border to mange the mouse movement

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Board variable initialization
+
 var w1 = document.getElementsByClassName("w1");
 var w2 = document.getElementsByClassName("w2");
 var w3 = document.getElementsByClassName("w3");
@@ -7,3 +9,5 @@ var y3 = document.getElementsByClassName("y3");
 var z1 = document.getElementsByClassName("z1");
 var z2 = document.getElementsByClassName("z2");
 var z3 = document.getElementsByClassName("z3");
+
+// 
