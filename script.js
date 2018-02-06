c724bdf569601ad061f67595a7fc143809106d02
Jay Guzman <j.d.guzman.v@gmail.com>
2018-02-06 12:03:32 -0700

    First try to add interactive functionalities to the TicTacToe board, it should display the X or O, before choosing a certain position, to give the player some visibility

--- script.js
+++ script.js
@@ -10,4 +10,37 @@ var z1 = document.getElementsByClassName("z1");
 var z2 = document.getElementsByClassName("z2");
 var z3 = document.getElementsByClassName("z3");
 
-// 
+
+
+
+//Functions
+
+//This Function handles the exact position of the mouse,
+
+ function mouseFunction(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    var coor = "Coordinates: (" + x + "," + y + ")";
+    console.log(coor);
+
+    var  = 'x';
+
+    var element = {};
+
+
+      if(y > 110 && y < 269 && x > 517 && x < 684 ){
+        element = document.getElementById("w1");
+        element.classList.add(sayx);
+      }
+
+      else if(y > 271 && y < 449 && x > 517 && x < 684){
+
+        element = document.getElementById("y1");
+        element.classList.add("x");
+        console.log(element);
+      }
+
+
+
+
+}
